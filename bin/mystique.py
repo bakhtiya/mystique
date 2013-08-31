f3df5cee343ddf2f860d8a8ad6c323df0c981757
Cyrus Bakhtiyari <me@cyrusbakhtiyari.com>
2013-08-30 22:35:39 -0500

    Modularized into seperate files.

--- bin/mystique.py
+++ bin/mystique.py
@@ -12,64 +12,16 @@
 # by periodically refreshing the new added list of torrents and looking for the
 # best torrents to seed to gain the best ratio :p MUHAHHAHHAHA
 
-# imports
+# general imports
 import sys, os
-from pycurl import Curl
-from cStringIO import StringIO
-from copy import copy
-from random import randint as rnd
+
+# modify path
+sys.path.append("lib")
+
+# specific imports
 from time import sleep
-
-# globals
-defaultTimeout = 5 # seconds for timeout (default)
-connectTimeout = 5 # seconds for timeout (connection timeout)
-userAgents =	[
-					"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.57 Safari/537.36"
-				]
-defaultHeaders =	[
-						"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-						"Accept-Language: en-US,en;q=0.8"
-					]
-
-# functions
-def getUserAgent():
-	'''Grab and return a random user agent from the global user agents list.'''
-	return userAgents[rnd(0, rnd(0,len(userAgents) - 1))]
-
-def post(url, data="", headers=[]):
-        '''Given a URL, send a post request with post data and headers. Return the response.'''
-        b = StringIO()
-        c = Curl()
-        c.setopt(c.URL, url)
-        c.setopt(c.POST, True)
-        c.setopt(c.POSTFIELDS, data)
-        c.setopt(c.FOLLOWLOCATION, True)
-        c.setopt(c.WRITEFUNCTION, b.write)
-        c.setopt(c.VERBOSE, True)
-        c.setopt(c.CONNECTTIMEOUT, connectTimeout)
-        c.setopt(c.TIMEOUT, defaultTimeout)
-        c.setopt(c.USERAGENT, getUserAgent())
-        c.setopt(c.HTTPHEADER, defaultHeaders + headers)
-        c.perform()
-        r = copy(b.getvalue())
-        b.close()
-        return r
-
-def get(url, headers=[]):
-        '''Given a URL and headers send a HTTP GET request. Return the response.'''
-        b = StringIO()
-        c = Curl()
-        c.setopt(c.URL, url)
-        c.setopt(c.FOLLOWLOCATION, True)
-        c.setopt(c.WRITEFUNCTION, b.write)
-        c.setopt(c.CONNECTTIMEOUT, connectTimeout)
-        c.setopt(c.TIMEOUT, defaultTimeout)
-        c.setopt(c.USERAGENT, getUserAgent())
-        c.setopt(c.HTTPHEADER, defaultHeaders + headers)
-        c.perform()
-        r = copy(b.getvalue())
-        b.close()
-        return r
+from console import console
+from url import *
 
 # main handler
 if __name__ == "__main__":
@@ -78,11 +30,16 @@ if __name__ == "__main__":
 	#print get("addictivemobility.com/headers.php")
 
 	# make the bitsoup login initial call
-	get("https://www.bitsoup.me/login.php")
+        console("calling bitsoup login page")
+        get("https://www.bitsoup.me/login.php")
+        console("bitsoup login page called")
 
 	# sleep to avoid being caught as a bot
 	# one cannot simply ask for login page, type his username and password in 0.0001 seconds :p
+        console("sleeping for 5 seconds")
 	sleep(5)
 
 	# make the login request
-	print post("https://www.bitsoup.me/takelogin.php")
+        console("sending login request to bitsoup")
+        post("https://www.bitsoup.me/takelogin.php", data="username=test&password=test")
+        console("login request sent")
