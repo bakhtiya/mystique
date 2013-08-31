cfaa899e97fb02366a10215570942a9f2c3f7535
Cyrus Bakhtiyari <me@cyrusbakhtiyari.com>
2013-08-30 22:43:18 -0500

    Added cookie path option.

--- lib/config.py
+++ lib/config.py
@@ -23,6 +23,7 @@ connectTimeout = int(config.get("connect_timeout", 5))
 userAgent = str(config.get("user_agent", "Mozilla/5.0 (compatible; MSIE 10.6; Windows NT 6.1; Trident/5.0; InfoPath.2; SLCC1; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET CLR 2.0.50727) 3gpp-gba UNTRUSTED/1.0")) # IE 10.6 UA
 defaultHeaders = config.get("default_headers", [])
 verbose = bool(config.get("verbose", False))
+cookiePath = str(config.get("cookie_path", "data/cookies.txt"))
 
 # format parameters (json treats everything as unicode, when it starts at a str (wierd eh?))
 for index in range(len(defaultHeaders)):
